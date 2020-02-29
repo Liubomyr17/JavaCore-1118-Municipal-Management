9c16246f0c439a7d258d0909522edb77366a1172
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-29 11:07:25 +0200

    Add files via upload

---
+++ JavaCore 1118 Municipal Management/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+/*
+
+1118 Municipal Management
+Arrange correctly the “chain of inheritance” in the classes: Pet (pet), Cat (cat), Dog (dog), Car (car).
+Requirements:
+1. In the class Solution there must be a public class Pet (pet).
+2. In the class Solution there must be a public class Cat (cat).
+3. The Solution class must have a public Dog class.
+4. In the class Solution there must be a public class Car (machine).
+5. Cat must be inherited from Pet.
+6. The class Dog must be inherited from the class Pet.
+7. The Pet class should not be inherited from our classes.
+8. The Car class should not be inherited from our classes.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    public class Pet {
+    }
+    public class Cat extends Pet {
+    }
+    public class Car {
+    }
+    public class Dog extends Pet {
+    }
+}
